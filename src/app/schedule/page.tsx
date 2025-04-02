7e247c08211e9c74c1925ef4215eda85d81d7e75
hyunho <hyunho@telcoware.com>
2025-04-02 12:53:08 +0900

    update 스케쥴 링크

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -6,14 +6,13 @@ export default function Schedule() {
             <h1>Schedule</h1>
             {/* 모바일 버전 */}
             <div className="md:hidden w-screen h-[calc(100vh-64px)] overflow-hidden">
-                <iframe
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSHnsk8Et01HHrkvQ7hvEFvnlYLQZyiRjp6sYLKs2t9pw5lLoQTlwkidC-cdaDR_4GYoK76AIONq_7l/pubhtml?gid=720224361&amp;single=true&amp;widget=true&amp;headers=false"
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSDFFurOk2Pcb2YeZOmjripo4yAr9Lm4A5-tAqqOVfnCNFUlWbiLN4ho7uNzbTOAK2RavAU5nwwxO5F/pubhtml?gid=1709044788&amp;single=true&amp;widget=true&amp;headers=false"
                     className="w-full h-full border-none absolute inset-0"
                 />
             </div>
             {/* 데스크탑 버전 */}
             <div className="hidden md:block fixed top-16 left-0 right-0 bottom-0 w-screen h-[calc(100vh-64px)] overflow-hidden">
-                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSHnsk8Et01HHrkvQ7hvEFvnlYLQZyiRjp6sYLKs2t9pw5lLoQTlwkidC-cdaDR_4GYoK76AIONq_7l/pubhtml?gid=100395762&amp;single=true&amp;widget=true&amp;headers=false"
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSDFFurOk2Pcb2YeZOmjripo4yAr9Lm4A5-tAqqOVfnCNFUlWbiLN4ho7uNzbTOAK2RavAU5nwwxO5F/pubhtml?gid=1168438816&amp;single=true&amp;widget=true&amp;headers=false"
                     className="w-full h-full border-none absolute inset-0"
                 />
             </div>
