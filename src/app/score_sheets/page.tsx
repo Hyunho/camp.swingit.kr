3329d44f20123167cf9e427a1521c867b2660238
hyunho <hyunho@telcoware.com>
2025-04-11 15:17:20 +0900

    fix spell error

--- src/app/score_sheets/page.tsx
+++ src/app/score_sheets/page.tsx
@@ -9,7 +9,7 @@ export default function ScoreSheets() {
             intro="">
             <div className="flex flex-col gap-8">
                 <div className="flex flex-col gap-4">
-                    <h2 className="text-2xl font-bold">KL2025</h2>
+                    <h2 className="text-2xl font-bold">KLHC 2025</h2>
                     <a href="https://docs.google.com/spreadsheets/d/1YCoj-h8wJbMlC1OxHvSEwvXIOBtOXmz3/edit?usp=sharing&ouid=107971928372041626299&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
                         Link to the 2025 KLHC score sheet results
                     </a>
