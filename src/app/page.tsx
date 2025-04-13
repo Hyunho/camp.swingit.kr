0b1a6bc05076c93a8eafc2d3619d6342132ec398
hyunho <oksknoh@gmail.com>
2025-04-13 23:02:48 +0900

    스태프 링크를 홈화면에서 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1135,29 +1135,7 @@ export default async function Home() {
                 </div>
                 <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5" />
               </div> */}
-              <Link href="/staffs" className="hover:text-blue-700 block mt-2">
-
-                <div className="relative lg:col-span-2">
-                  <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
-                  <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
-                    {/* <img
-      alt=""
-      src="https://tailwindcss.com/plus-assets/img/component-images/bento-01-network.png"
-      className="h-80 object-cover"
-    /> */}
-                    <div className="p-10 pt-4">
-                      <h3 className="text-xl font-semibold text-indigo-600">Staffs</h3>
-                      <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Meet the amazing team behind Camp Swing It 2025!</p>
-                      <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-                        Meet the amazing team behind Camp Swing It 2025! We sincerely thank all staff members for their dedication and hard work in making this event possible. You are the heart of our event!
-                      </p>
-                      <Link href="/staffs" className="text-blue-500 hover:text-blue-700 block mt-2">Go to Staffs page</Link>
-                    </div>
-                  </div>
-                  <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
-                </div>
-              </Link>
-              <Link href="/photos" className="hover:text-blue-700 block mt-2">
+              <Link href="/albums" className="hover:text-blue-700 block mt-2">
                 <div className="relative lg:col-span-2">
                   <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
                   <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
